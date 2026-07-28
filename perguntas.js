13a1a2337b6132d020447476e2c179e07d8ab79b
joaojaguara <joao.batista.silva23@escola.pr.gov.br>
2026-07-28 11:07:48 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -2,10 +2,15 @@
 // Troque disciplina, pergunta e resposta pelos conteúdos que você criou.
 
 criaCartao(
-   'Disciplina',
-    'Pergunta',
-    'Resposta'
-)
-
-
-
+   'matematica'
+    'Quanto é 8 × 7?'
+    '56.'
+'matematica'
+    'Quanto é 9 × 6?'
+    '54.'
+'Português'
+    'O que é um verbo?'
+    'É a palavra que indica ação, estado ou fenômeno da natureza'
+'Português'
+    ''
+    ''
