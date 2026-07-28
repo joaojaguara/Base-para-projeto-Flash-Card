9ff411d5a3b7eb2c585308acadb50702a5df802e
joaojaguara <joao.batista.silva23@escola.pr.gov.br>
2026-07-28 11:19:34 -0300

    a 01

--- perguntas.js
+++ perguntas.js
@@ -5,12 +5,48 @@ criaCartao(
    'matematica'
     'Quanto é 8 × 7?'
     '56.'
+criaCartao(
 'matematica'
     'Quanto é 9 × 6?'
     '54.'
+criaCartao(
 'Português'
     'O que é um verbo?'
     'É a palavra que indica ação, estado ou fenômeno da natureza'
+criaCartao(
 'Português'
+    'Na frase "Ele correu muito", qual é a função da palavra "muito"?'
+    'Advérbio de intensidade'
+criaCartao(
+   'História',
+    'Qual evento deu início à Idade Moderna em 1453?',
+    'A Queda de Constantinopla.'
+criaCartao(
+) 'História',
+    ' Quem foi o primeiro presidente do Brasil?',
+    ' Marechal Deodoro da Fonseca.'
+criaCartao(
+ 'Biologia',
+    'Qual molécula carDNA (Ácido Desoxirribonucleico).rega as instruções genéticas de um organismo?',
+    'DNA (Ácido Desoxirribonucleico).'
+criaCartao(
+ 'Biologia',
+    ' Qual é o principal pigmento responsável por captar a luz solar na fotossíntese?',
+    'Clorofila.'
+criaCartao(
+ 'Química',
+    ' Qual é a fórmula química da água?',
+    'H₂O'
+criaCartao(
+   ''
     ''
     ''
+criaCartao(
+   ''
+    ''
+    ''
+criaCartao(
+   ''
+    '' 
+    '.'
+
